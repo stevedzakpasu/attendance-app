9b26352784fbcd0f872973e8b5e7da4592b43866
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2022-12-26 16:41:14 +0000

    updated requirements file

--- database.py
+++ database.py
@@ -10,5 +10,4 @@ url = os.getenv('DATABASE_URI')
 
 
 # engine = create_engine(url, echo=True)
-engine = create_engine(
-    "postgresql://edbqpqgpzyumgs:41c4a5f9d1f4e7345649cb25046bfaf5f3f9cef4a75321c970b6be70f139ac9a@ec2-52-201-124-168.compute-1.amazonaws.com:5432/dal3la68nt4bsb", echo=True)
+engine = create_engine(url, echo=True)
